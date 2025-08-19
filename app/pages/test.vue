bc005b4dbf5102d0bd1b37f6a2780101520b6e15
msolanogithub <msolano2505@gmail.com>
2025-08-19 18:53:19 -0500

    feat: refactor api-fetch plugin and clean up test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,11 +1,9 @@
+<script setup>
+
+</script>
+
 <template>
   <div>
     <h1>Test Page...</h1>
   </div>
 </template>
-
-<script setup>
-definePageMeta({
-  permission: ['auth'],
-})
-</script>
--- app/plugins/api-fetch.ts
+++ app/plugins/api-fetch.ts
@@ -1,11 +1,13 @@
-export default defineNuxtPlugin(() =>
-{
-  const apiFetch = $fetch.create({
-    baseURL: useRuntimeConfig().public.apiBase,
-  })
-
-  // Expose to useNuxtApp().$apiFetch
-  return {
-    provide: {apiFetch}
+export default defineNuxtPlugin({
+  name: "api-fetch",
+  async setup ()
+  {
+    const apiFetch = $fetch.create({
+      baseURL: useRuntimeConfig().public.apiBase,
+    })
+    // Expose to useNuxtApp().$apiFetch
+    return {
+      provide: {apiFetch}
+    }
   }
 })
