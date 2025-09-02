40dfebe77f460a97a5b3690a3731b63b2d0b92cd
adrianacardoso <acardosof27@gmail.com>
2025-09-02 10:07:08 -0400

    +update: imedia

--- app/components/CardHome.vue
+++ app/components/CardHome.vue
@@ -1,12 +1,20 @@
 <!-- components/ItemCard.vue -->
 <script setup lang="ts">
-defineProps<{ item: { id: number; category: string; title: string; image: string } }>()
+defineProps<{ item: { id: number; category: string; title: string; media: object } }>()
 </script>
 
 <template>
   <div class="h-full rounded-xl">
-    <img :src="item.image" :alt="item.title" class="w-full aspect-4/3 object-cover rounded-xl">
+    <IMediaRender
+      :media="item.media" :alt="item.title"
+      aspect-ratio="aspect-4/3"
+      :ui="{
+        wrapper: '',
+        container: '',
+        media: 'w-full  object-cover rounded-xl'
+    }" />
     <div class="text-center text-primary text-[16px] lg:text-[20px] font-semibold mt-4">{{ item.title  }}</div>
-    <div class="text-center text-secondary text-[14px] lg:text-[16px] uppercase">{{ item.category  }}</div>
+    <div class="text-center text-secondary text-[14px] lg:text-[16px] uppercase mb-1">{{ item.category  }}</div>
+    <UButton size="xs" color="secondary">Reserva ahora</UButton>
   </div>
 </template>
