ea2449a2e04fb3167820e03e82c568a61618d25e
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2025-09-15 16:57:31 -0500

    +update: enhance SEO metadata for improved social sharing

--- modules/ipage/runtime/pages/[page-slug].vue
+++ modules/ipage/runtime/pages/[page-slug].vue
@@ -4,11 +4,20 @@ const pageSlug = route.params.slug?.toString() || 'inicio'
 const {page, PageComponent} = await usePageLoader(pageSlug)
 const settingStore = useIsettingStore()
 
+
+
 useSeoMeta({
   title: () => `${page.value?.title} | ${settingStore.get('isite::siteName')}`,
   ogTitle: () => page.value?.title,
   description: () => page.value?.ogDescription,
-  ogDescription: () => page.value?.ogDescription
+  ogDescription: () => page.value?.ogDescription,
+  ogImage: ()=>page.value?.ogImage,
+  ogType: ()=>page.value?.ogType,
+  twitterTitle: () => page.value?.title,
+  twitterDescription: () => page.value?.ogDescription,
+  twitterImage: () => page.value?.ogImage,
+
+
 })
 </script>
 
