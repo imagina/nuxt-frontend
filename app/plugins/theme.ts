c5e31077aedde31b837e7a24fed8456e87ea828d
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2025-09-17 14:54:15 -0500

    feat: enhance theme plugin with additional contrast color variables

--- app/plugins/theme.ts
+++ app/plugins/theme.ts
@@ -4,14 +4,24 @@ export default defineNuxtPlugin({
     const settingStore = useIsettingStore()
 
     const primaryColor = settingStore.get("icore::brandPrimary")
+    const primaryContrast = settingStore.get("icore::primaryContrast")
     const secondaryColor = settingStore.get("icore::brandSecondary")
+    const secondaryContrast = settingStore.get("icore::secondaryContrast")
     const tertiaryColor = settingStore.get("icore::brandTertiary")
+    const tertiaryContrast = settingStore.get("icore::tertiaryContrast")
+    const quaternaryColor = settingStore.get("icore::quaternaryColor")
+    const quaternaryContrast = settingStore.get("icore::quaternaryContrast")
 
     const cssVars = `
       :root {
         ${primaryColor ? `--color-primary: ${primaryColor};` : ""}
+        ${primaryContrast ? `--color-primary-contrast: ${primaryContrast};` : ""}
         ${secondaryColor ? `--color-secondary: ${secondaryColor};` : ""}
+        ${secondaryContrast ? `--color-secondary-contrast: ${secondaryContrast};` : ""}
         ${tertiaryColor ? `--color-tertiary: ${tertiaryColor};` : ""}
+        ${tertiaryContrast ? `--color-tertiary-contrast: ${tertiaryContrast};` : ""}
+        ${quaternaryColor ? `--color-quaternary: ${quaternaryColor};` : ""}
+        ${quaternaryContrast ? `--color-quaternary-contrast: ${quaternaryContrast};` : ""}
       }
     `
 
