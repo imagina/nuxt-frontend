36c1a7a949f32f6f3dadd18d092cbdb707a16203
adrianacardoso <acardosof27@gmail.com>
2025-10-07 09:59:40 -0400

    +update: nosotros laboratorio

---
+++ app/pages/page-9.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import type { PageData } from '#ipage/types/pages'
+defineProps<{ page: PageData }>()
+
+
+
+</script>
+
+<template>
+  <!-- Nosotros Laboratorio -->
+  <IBreadcrumb
+      :title="page.title ?? '...'"
+      :ui="{ link: 'font-bold text-gray-3' }">
+  </IBreadcrumb>
+  <UContainer class="py-10 nosotros ">
+    <h1 class="page-title hidden">{{ page.title }}</h1>
+    <div class="page-body-img">
+      <!-- Imagen primero -->
+      <IMediaRender
+        :media="page.files.mainimage"
+        :alt="page.title"
+        aspect-ratio="auto"
+        :ui="{
+          wrapper: 'float-right w-full lg:w-1/2 h-[450px] ml-10 mb-10',
+          media: 'w-full h-full object-cover rounded-2xl',
+        }"
+      />
+      <div class="page-body text-justify" v-html="page.body"></div>
+    </div>
+  </UContainer>
+
+  <div class="w-full h-2"></div>
+
+  <!-- Slider con video us -->
+  <ISliderVideoUs name="asfalcargo_nosotros_video"></ISliderVideoUs>
+
+  <!-- Carousel Clientes -->
+  <ISliderClients name="colbitumen_clientes"></ISliderClients>
+
+</template>
+<style scoped>
+:deep(.page-body) {
+  & h2 { position: relative; }
+  & h2:before {
+    content: "";
+    display: block;
+    background-color: var(--color-tertiary);
+    width: 96px;
+    height: 4px;
+    position: absolute;
+    bottom: -18px;
+  }
+}
+</style>
